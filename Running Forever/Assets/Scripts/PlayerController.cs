2642f04004336935483efaaa5f103b1f06fded06
AnonymousApplicant <44824998+AnonymousApplicant@users.noreply.github.com>
2019-12-13 14:16:54 +0800

    Updated Player Controller from CharacterController to Rigidbody

--- Running Forever/Assets/Scripts/PlayerController.cs
+++ Running Forever/Assets/Scripts/PlayerController.cs
@@ -4,38 +4,26 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    private CharacterController controller;
-
-    [SerializeField]
-    private float gravity; // Variable that defines the gravity force
+    private Rigidbody rb;
 
     private bool canJump;
     [SerializeField]
     private float jumpHeight; // Variable that defines the jump height
 
-    private float velocityY; // variable for gravity velocity
-
     void Start()
     {
-        controller = GetComponent<CharacterController>();
+        rb = GetComponent<Rigidbody>();
     }
 
     void FixedUpdate()
     {
-        // Update velocity Y by gravity variable
-        velocityY += (Time.fixedDeltaTime * 2) * gravity;
-
-        if (controller.isGrounded && velocityY < gravity / 2) velocityY = gravity / 2;
-
-        controller.Move(new Vector3(0f, velocityY, 0f) * Time.deltaTime);
-
         if (Input.touchCount > 0)
         {
             Touch touch = Input.GetTouch(0);
 
             if (touch.phase == TouchPhase.Began)
             {
-                if (controller.isGrounded)
+                if (canJump)
                 {
                     Jump();
                 }
@@ -46,7 +34,11 @@ public class PlayerController : MonoBehaviour
     void Jump()
     {
         canJump = false;
-        float jumpVelocity = Mathf.Sqrt(-2 * gravity * (jumpHeight * 2));
-        velocityY = jumpVelocity;
+        rb.AddForce(new Vector3(0f, jumpHeight, 0f), ForceMode.VelocityChange);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        canJump = true;
     }
 }
