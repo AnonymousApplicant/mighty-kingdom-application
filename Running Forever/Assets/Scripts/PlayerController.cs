b680d440ec10a9e60f3a5dc39756ae7036d7db32
AnonymousApplicant <44824998+AnonymousApplicant@users.noreply.github.com>
2019-12-13 13:58:23 +0800

    Updated Player Controller script

--- Running Forever/Assets/Scripts/PlayerController.cs
+++ Running Forever/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,49 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private CharacterController controller;
+
+    [SerializeField]
+    private float gravity; // Variable that defines the gravity force
+
+    private bool canJump;
+    [SerializeField]
+    private float jumpHeight; // Variable that defines the jump height
+
+    private float velocityY; // variable for gravity velocity
+
     void Start()
     {
-        
+        controller = GetComponent<CharacterController>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        // Update velocity Y by gravity variable
+        velocityY += (Time.fixedDeltaTime * 2) * gravity;
+
+        if (controller.isGrounded && velocityY < gravity / 2) velocityY = gravity / 2;
+
+        controller.Move(new Vector3(0f, velocityY, 0f) * Time.deltaTime);
+
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            if (touch.phase == TouchPhase.Began)
+            {
+                if (controller.isGrounded)
+                {
+                    Jump();
+                }
+            }
+        }
+    }
+
+    void Jump()
+    {
+        canJump = false;
+        float jumpVelocity = Mathf.Sqrt(-2 * gravity * (jumpHeight * 2));
+        velocityY = jumpVelocity;
     }
 }
